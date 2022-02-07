4c40ebdaf8750fcc7178c00e20ba26bfc34c945c
MartaBenitez <92039488+MartaBenitez@users.noreply.github.com>
2022-02-07 09:37:07 +0100

    Add files via upload

---
+++ js/funciones2.js
@@ -0,0 +1,4 @@
+function cargarPagina()
+{
+	document.getElementsByTagName("body")[0].innerHTML="<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet sodales enim, ac fringilla tellus. Vestibulum fermentum dapibus erat eu tempor. Nam convallis posuere quam, sed vehicula turpis vulputate vitae. Sed ac arcu pellentesque, ultrices purus nec, ultricies quam. Maecenas ullamcorper pellentesque quam in venenatis. Fusce blandit feugiat est non condimentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos </p>";
+}
